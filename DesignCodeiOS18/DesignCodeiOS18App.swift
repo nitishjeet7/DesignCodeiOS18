f618835dac88c444d4673d29a4638768864a4b0c
nitish <nitishjeet7@gmail.com>
2024-07-31 12:42:11 +0530

    Initial Commit

---
+++ DesignCodeiOS18/DesignCodeiOS18App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeiOS18App.swift
+//  DesignCodeiOS18
+//
+//  Created by Nitish on 31/07/24.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeiOS18App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
